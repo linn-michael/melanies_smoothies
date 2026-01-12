d332d4ea41e83c8fa9f537e4c8f39e11d1e8d873
Michael Linn <linn.michael182@yahoo.com>
2026-01-12 15:10:54 -0600

    Add Streamlit app for customizing smoothies

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Get the current credentials
+session = get_active_session()
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+  "Choose the fruits you want in your custom Smoothie!"
+)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+data = session.table("smoothies.public.fruit_options")
+
+ingredients_list = st.multiselect("Choose up to 5 ingredients",
+                      data.select(col('FRUIT_NAME')),
+                       max_selections=5)
+ 
+ingredients_string = ' '.join(ingredients_list)
+
+ 
+if ingredients_string and st.button('Submit'):
+    my_insert_stmt = """ insert into smoothies.public.orders (name_on_order, ingredients)
+                values ('""" + name_on_order + """', '""" + ingredients_string + """')
+    """
+    session.sql(my_insert_stmt).collect()
+    st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
